6b156260e2736c225d5a8f4d50bea085f0ef83fe
kashif-hadiftech <kashif@hadiftech.com>
2019-08-24 19:30:25 +0500

    Signup Type Page UI Completed

--- app/src/main/kotlin/com/hadiftech/hamba/features/login/LoginActivity.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/features/login/LoginActivity.kt
@@ -1,13 +1,21 @@
 package com.hadiftech.hamba.features.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.hadiftech.hamba.R
 import com.hadiftech.hamba.core.HambaBaseActivity
+import com.hadiftech.hamba.features.signup.JoinUsActivity
 
 class LoginActivity : HambaBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onCreateAccountClicked(createAccountTextView: View){
+        val joinUsIntent = Intent(this, JoinUsActivity::class.java)
+        startActivity(joinUsIntent)
     }
 }
---
+++ app/src/main/kotlin/com/hadiftech/hamba/features/signup/JoinUsActivity.kt
@@ -0,0 +1,13 @@
+package com.hadiftech.hamba.features.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hadiftech.hamba.R
+
+class JoinUsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_us)
+    }
+}
