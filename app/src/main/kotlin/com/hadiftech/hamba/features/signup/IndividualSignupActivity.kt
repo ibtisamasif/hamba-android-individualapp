059913cd660502605628d3c45ddb6a3226716d14
kashif-hadiftech <kashif@hadiftech.com>
2019-08-25 21:26:25 +0500

    Individual Signup Page In progress

---
+++ app/src/main/kotlin/com/hadiftech/hamba/features/signup/IndividualSignupActivity.kt
@@ -0,0 +1,13 @@
+package com.hadiftech.hamba.features.signup
+
+import android.os.Bundle
+import com.hadiftech.hamba.R
+import com.hadiftech.hamba.core.HambaBaseActivity
+
+class IndividualSignupActivity : HambaBaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_individual_signup)
+    }
+}
