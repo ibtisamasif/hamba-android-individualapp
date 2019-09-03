7a806be946aed7d4cdce625994d824710a1a71a6
Ibtisam Asif <ibtisamasif@gmail.com>
2019-09-04 01:23:58 +0500

    Banner images changing at some intervals

--- app/src/main/kotlin/com/hadiftech/hamba/features/home/HomeFragment.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/features/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.hadiftech.hamba.features.home
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -23,6 +25,17 @@ class HomeFragment : Fragment() {
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.tvCurrentLocation)
+
+        val animation = AnimationDrawable()
+        animation.addFrame(resources.getDrawable(R.drawable.image_1), 2000)
+        animation.addFrame(resources.getDrawable(R.drawable.image_2), 2500)
+        animation.addFrame(resources.getDrawable(R.drawable.image_3), 3000)
+        animation.isOneShot = false
+        val tvBanner: ImageView = root.findViewById(R.id.tvBanner)
+        tvBanner.setBackgroundDrawable(animation)
+        // start the animation!
+        animation.start()
+
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
