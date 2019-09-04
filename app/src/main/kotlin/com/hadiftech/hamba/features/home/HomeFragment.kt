7a6ec3970cfc0f6125ac43bced7efb8f1fd4ca52
Ibtisam Asif <ibtisamasif@gmail.com>
2019-09-04 11:39:18 +0500

    top deals animation

--- app/src/main/kotlin/com/hadiftech/hamba/features/home/HomeFragment.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/features/home/HomeFragment.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.TranslateAnimation
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -25,6 +29,9 @@ class HomeFragment : Fragment() {
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.tvCurrentLocation)
+        homeViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
 
         val animation = AnimationDrawable()
         animation.addFrame(resources.getDrawable(R.drawable.image_1), 2000)
@@ -36,9 +43,14 @@ class HomeFragment : Fragment() {
         // start the animation!
         animation.start()
 
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        val translateAnimation: Animation = TranslateAnimation(TranslateAnimation.ABSOLUTE, 0f, TranslateAnimation.ABSOLUTE, 1000f, TranslateAnimation.ABSOLUTE, 0f, TranslateAnimation.ABSOLUTE, 0f)
+        translateAnimation.duration = 10000
+        translateAnimation.repeatCount = -1
+        translateAnimation.repeatMode = Animation.REVERSE
+        translateAnimation.interpolator = LinearInterpolator()
+        val innerLay: LinearLayout = root.findViewById(R.id.innerLay)
+        innerLay.animation = translateAnimation
+
         return root
     }
 }
