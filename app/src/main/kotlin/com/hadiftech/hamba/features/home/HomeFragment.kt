2c5abded6883c191989e7bdaec26cad40cb85277
Ibtisam Asif <ibtisamasif@gmail.com>
2019-09-05 01:15:10 +0500

    refactoring

--- app/src/main/kotlin/com/hadiftech/hamba/features/home/HomeFragment.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/features/home/HomeFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
 import android.view.animation.TranslateAnimation
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -28,19 +30,19 @@ class HomeFragment : HambaBaseFragment() {
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
-
-        bannerAnimation()
+        bannerAnimation(root)
+        val llHorizontalScroll: LinearLayout = root.findViewById(R.id.llHorizontalScroll)
         llHorizontalScroll.animation = topDealsAnimation()
-
         return root
     }
 
-    private fun bannerAnimation(){
+    private fun bannerAnimation(root: View){
         val animation = AnimationDrawable()
                 animation.addFrame(resources.getDrawable(R.drawable.top_deals_image_1), 2000)
         animation.addFrame(resources.getDrawable(R.drawable.top_deals_image_2), 2500)
         animation.addFrame(resources.getDrawable(R.drawable.top_deals_image_3), 3000)
         animation.isOneShot = false
+        val ivBanner: ImageView = root.findViewById(R.id.ivBanner)
         ivBanner.setBackgroundDrawable(animation)
         animation.start()
     }
