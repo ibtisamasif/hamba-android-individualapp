5e1d514612826a53890ac84defc826641172fb1d
Ibtisam Asif <ibtisamasif@gmail.com>
2019-09-06 23:38:08 +0500

    profile screen ui

--- app/src/main/kotlin/com/hadiftech/hamba/features/profile/ProfileActivity.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/features/profile/ProfileActivity.kt
@@ -1,8 +1,19 @@
 package com.hadiftech.hamba.features.profile
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.hadiftech.hamba.R
 import com.hadiftech.hamba.core.HambaBaseActivity
 
 class ProfileActivity: HambaBaseActivity(){
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
 
+    fun onSaveButtonClicked(bSave: View) {
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+    }
 }
