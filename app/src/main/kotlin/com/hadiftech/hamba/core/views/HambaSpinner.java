08b3b45ee1756f9a8a8641a67bc00bbd97a69787
Ibtisam Asif <ibtisamasif@gmail.com>
2019-09-11 00:25:29 +0500

    manage profile ui

---
+++ app/src/main/kotlin/com/hadiftech/hamba/core/views/HambaSpinner.java
@@ -0,0 +1,43 @@
+package com.hadiftech.hamba.core.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.ArrayRes;
+import androidx.appcompat.widget.AppCompatSpinner;
+
+import com.hadiftech.hamba.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HambaSpinner extends AppCompatSpinner {
+
+    public HambaSpinner(Context context) {
+        super(context);
+    }
+
+    public HambaSpinner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void populate(Context context, int layout, String item) {
+        List<String> list = new ArrayList<>();
+        list.add(item);
+        populate(context, layout, list);
+    }
+
+    public void populate(Context context, int layout, List<String> spinnerItems) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(context, layout, spinnerItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.setAdapter(adapter);
+    }
+
+    public void populate(Context context, @ArrayRes int textArrayResId) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context, textArrayResId, R.layout.spinner_item_dark);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.setAdapter(adapter);
+    }
+
+}
--- app/src/main/kotlin/com/hadiftech/hamba/features/profile/ProfileActivity.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/features/profile/ProfileActivity.kt
@@ -5,12 +5,63 @@ import android.view.View
 import android.widget.Toast
 import com.hadiftech.hamba.R
 import com.hadiftech.hamba.core.HambaBaseActivity
+import kotlinx.android.synthetic.main.activity_profile.*
+
 
 class ProfileActivity: HambaBaseActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        populatePersonTypeDropDown()
+        populatePrefixDropDown()
+        populateNationalityDropDown()
+        populateIdentityDropDown()
+        populateCountryDropDown()
+        populateAddressTypeDropDown()
+        populateInterestDropDown()
+    }
+
+    private fun populatePersonTypeDropDown() {
+        val items = ArrayList<String>()
+        items.add("Service Provider")
+        spPersonType.populate(this, R.layout.spinner_item_dark, items)
+    }
+
+    private fun populatePrefixDropDown() {
+        val items = ArrayList<String>()
+        items.add("Mr.")
+        spPrefix.populate(this, R.layout.spinner_item_light, items)
+    }
+
+    private fun populateNationalityDropDown() {
+        val items = ArrayList<String>()
+        items.add("Select Nationality")
+        spNationality.populate(this, R.layout.spinner_item_dark, items)
+    }
+
+    private fun populateIdentityDropDown() {
+        val items = ArrayList<String>()
+        items.add("Select Identity")
+        spIdentity.populate(this, R.layout.spinner_item_dark, items)
+    }
+
+    private fun populateCountryDropDown() {
+        val items = ArrayList<String>()
+        items.add("Select Country")
+        spCountry.populate(this, R.layout.spinner_item_dark, items)
+    }
+
+    private fun populateAddressTypeDropDown() {
+        val items = ArrayList<String>()
+        items.add("Select Address Type")
+        spAddressType.populate(this, R.layout.spinner_item_dark, items)
+    }
+
+    private fun populateInterestDropDown() {
+        val items = ArrayList<String>()
+        items.add("Select Interest")
+        spInterest.populate(this, R.layout.spinner_item_dark, items)
     }
 
     fun onSaveButtonClicked(bSave: View) {
