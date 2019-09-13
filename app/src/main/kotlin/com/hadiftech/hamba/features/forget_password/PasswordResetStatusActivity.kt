f6cadad9cec7410eb2940baac5709d4f2098f170
kashif-hadiftech <kashif@hadiftech.com>
2019-09-13 13:06:37 +0500

    code refactored

--- app/src/main/kotlin/com/hadiftech/hamba/features/forget_password/PasswordResetStatusActivity.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/features/forget_password/PasswordResetStatusActivity.kt
@@ -1,13 +1,23 @@
 package com.hadiftech.hamba.features.forget_password
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.hadiftech.hamba.R
 import com.hadiftech.hamba.core.HambaBaseActivity
+import com.hadiftech.hamba.features.login.LoginActivity
 
 class PasswordResetStatusActivity : HambaBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_reset_status)
+    }
+
+    fun onLoginButtonClicked(loginBtn: View) {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        loginIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(loginIntent)
+        finish()
     }
 }
