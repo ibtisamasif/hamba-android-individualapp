e54d27829800e2bc57bd8de2ffd95289ed652b23
kashif-hadiftech <kashif@hadiftech.com>
2019-10-03 12:24:16 +0500

    Current Location Feature Integrated

--- app/src/main/kotlin/com/hadiftech/hamba/core/providers/AlertDialogProvider.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/core/providers/AlertDialogProvider.kt
@@ -1,10 +1,12 @@
 package com.hadiftech.hamba.core.providers
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.hadiftech.hamba.R
 import com.hadiftech.hamba.core.enums.DialogTheme
@@ -12,8 +14,10 @@ import com.hadiftech.hamba.core.listeners.DialogButtonClickListener
 import com.hadiftech.hamba.core.views.HambaButton
 import com.hadiftech.hamba.core.views.HambaTextView
 
+@SuppressLint("StaticFieldLeak")
 object AlertDialogProvider {
 
+    private var negativeButton: TextView? = null
     private var positiveButton: HambaButton? = null
     private var textViewTitle: HambaTextView? = null
     private var textViewMessage: HambaTextView? = null
@@ -44,6 +48,18 @@ object AlertDialogProvider {
         positiveButton!!.setOnClickListener { dialogButtonClickListener.onClick(alertDialog) }
     }
 
+    fun showAlertDialog(context: Context, theme: DialogTheme, message: String?, positiveBtnText: String?, positiveClickListener: DialogButtonClickListener,
+                        negativeClickListener: DialogButtonClickListener) {
+        val dialogView = getView(context, theme)
+        initializeViews(dialogView)
+        textViewMessage!!.text = message
+        positiveButton!!.text = positiveBtnText
+        negativeButton!!.visibility = View.VISIBLE
+        val alertDialog = createAlertDialog(context, dialogView)
+        positiveButton!!.setOnClickListener { positiveClickListener.onClick(alertDialog) }
+        negativeButton!!.setOnClickListener { negativeClickListener.onClick(alertDialog) }
+    }
+
     private fun getView(context: Context, theme: DialogTheme) : View {
         var layoutView: View? = null
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -57,6 +73,7 @@ object AlertDialogProvider {
     }
 
     private fun initializeViews(dialogView: View) {
+        negativeButton = dialogView.findViewById<TextView>(R.id.negativeBtn)
         positiveButton = dialogView.findViewById<HambaButton>(R.id.positiveBtn)
         textViewTitle = dialogView.findViewById<HambaTextView>(R.id.textView_title)
         textViewMessage = dialogView.findViewById<HambaTextView>(R.id.textView_message)
--- app/src/main/kotlin/com/hadiftech/hamba/features/home/HomeFragment.kt
+++ app/src/main/kotlin/com/hadiftech/hamba/features/home/HomeFragment.kt
@@ -1,13 +1,21 @@
 package com.hadiftech.hamba.features.home
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.drawable.AnimationDrawable
+import android.location.*
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
 import android.view.animation.TranslateAnimation
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.hadiftech.hamba.R
 import com.hadiftech.hamba.core.HambaBaseFragment
@@ -17,8 +25,17 @@ import com.hadiftech.hamba.core.session.Session
 import com.hadiftech.hamba.core.session.User
 import com.hadiftech.hamba.features.profile.get_profile_service.GetProfileResponse
 import kotlinx.android.synthetic.main.fragment_home.*
+import mumayank.com.airlocationlibrary.AirLocation
+import java.util.*
+import com.hadiftech.hamba.core.enums.DialogTheme
+import com.hadiftech.hamba.core.listeners.DialogButtonClickListener
+import com.hadiftech.hamba.core.providers.AlertDialogProvider
 
 class HomeFragment : HambaBaseFragment() {
+
+    private var airLocation: AirLocation? = null
+    private val LOCATION_PERMISSION_GRANTED: Int = 1111
+    private val GPS_ENABLE_REQUEST: Int = 2222
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
@@ -29,6 +46,105 @@ class HomeFragment : HambaBaseFragment() {
 
         bannerAnimation()
         llTopDealsHorizontalScroll.animation = topDealsAnimation()
+        checkCurrentLocationPermissions(true)
+    }
+
+    private fun checkCurrentLocationPermissions(displayAlert: Boolean) {
+        if ((ContextCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            && (ContextCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+
+            checkIsGpsEnabled(displayAlert)
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_GRANTED)
+        }
+    }
+
+    private fun checkIsGpsEnabled(displayAlert: Boolean) {
+
+        val locationManager = activity!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            fetchCurrentLocation()
+        } else {
+            if (displayAlert) {
+                displayGpsAlert()
+            } else {
+                textView_currentLocation.text = getString(R.string.gps_not_enabled)
+            }
+        }
+    }
+
+    private fun displayGpsAlert() {
+        AlertDialogProvider.showAlertDialog(context!!, DialogTheme.ThemeWhite, getString(R.string.enable_gps_description), getString(R.string.yes),
+            object : DialogButtonClickListener {
+                override fun onClick(alertDialog: AlertDialog) {
+                    alertDialog.dismiss()
+                    startActivityForResult(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), GPS_ENABLE_REQUEST)
+                }
+            },
+            object : DialogButtonClickListener {
+                override fun onClick(alertDialog: AlertDialog) {
+                    alertDialog.dismiss()
+                    textView_currentLocation.text = getString(R.string.gps_not_enabled)
+                }
+            })
+    }
+
+    private fun fetchCurrentLocation() {
+        airLocation = AirLocation(activity as Activity,
+            shouldWeRequestPermissions = false,
+            shouldWeRequestOptimization = false,
+            callbacks = object: AirLocation.Callbacks {
+
+                override fun onSuccess(location: Location) {
+                    displayLocation(location)
+                }
+
+                override fun onFailed(locationFailedEnum: AirLocation.LocationFailedEnum) {
+                    textView_currentLocation.text = getString(R.string.location_not_found)
+                }
+            })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            GPS_ENABLE_REQUEST -> {
+                checkCurrentLocationPermissions(false)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            LOCATION_PERMISSION_GRANTED -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkIsGpsEnabled(true)
+                } else {
+                    textView_currentLocation.text = getString(R.string.location_permission_rejected)
+                }
+            }
+        }
+    }
+
+    private fun displayLocation(location: Location?) {
+
+        if (location != null) {
+            val geoCoder = Geocoder(activity, Locale.getDefault())
+            val addressList = geoCoder.getFromLocation(location!!.latitude, location!!.longitude, 1)
+
+            if (addressList.isNotEmpty()) {
+                var address = addressList[0]
+                textView_currentLocation.text = address.subLocality + ", " + address.subAdminArea + ", " + address.countryCode
+            } else {
+                textView_currentLocation.text = getString(R.string.location_not_found)
+            }
+        } else {
+            textView_currentLocation.text = getString(R.string.location_not_found)
+        }
     }
 
     override fun onResume() {
